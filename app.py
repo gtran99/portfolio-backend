a855b9badb733d82d0ae035b6654accfb1926b4c
gtran99 <gavintran1@gmail.com>
2024-06-22 21:25:03 -0700

    Update project links

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 def get_portfolio():
     portfolio_data = {
         "name": "Gavin Tran",
-        "bio": "An avid learner.",
+        "bio": "An avid learner with 2 years of experience in Software Testing",
         "projects": [
-            {"title": "Project One", "description": "Description of project one.", "link": "#"},
-            {"title": "Project Two", "description": "Description of project two.", "link": "#"}
+            {"title": "Ecommerce Testing Framework",
+             "description": "Developed a test automation suite for an e-commerce website using Python and Flask", 
+             "link": "https://github.com/gtran99/Ecommerce-Test"},
+            {"title": "Recipe Finder", 
+             "description": "A full-stack web application using Flask and the Spoonacular API to find recipes based on selected ingredients", 
+             "link": "https://github.com/gtran99/RecipeFinder"}
         ]
     }
     return jsonify(portfolio_data)
